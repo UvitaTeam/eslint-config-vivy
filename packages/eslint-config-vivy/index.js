a764fda04c75a4520a26e42c765c38ac25ad6fec
christos <paschalidi@outlook.com>
2019-04-02 14:47:13 +0200

    removes underscore rule

--- packages/eslint-config-vivy/index.js
+++ packages/eslint-config-vivy/index.js
@@ -14,7 +14,6 @@ module.exports = {
     "class-methods-use-this": 0,
     "import/prefer-default-export": 0,
     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-    "prettier/prettier": ["error"],
-    "no-underscore-dangle": 0
+    "prettier/prettier": ["error"]
   }
 }
