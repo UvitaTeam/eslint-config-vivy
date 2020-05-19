c797834b8cef45df8ff4ee48205cb6b45115bc34
Ahmed Hassanein <ahmed.hassanein@vivy.com>
2020-05-19 13:45:37 +0200

    prettier

--- packages/eslint-config-vivy/index.js
+++ packages/eslint-config-vivy/index.js
@@ -1,24 +1,24 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   extends: [
-    'airbnb',
-    'prettier',
-    'prettier/react',
-    'plugin:react/recommended',
-    'plugin:jest/recommended',
+    "airbnb",
+    "prettier",
+    "prettier/react",
+    "plugin:react/recommended",
+    "plugin:jest/recommended",
   ],
-  plugins: ['prettier', 'jest', 'react'],
+  plugins: ["prettier", "jest", "react"],
   rules: {
-    'class-methods-use-this': 0,
-    'import/prefer-default-export': 0,
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.ts', '.jsx'] }],
-    'prettier/prettier': ['error'],
+    "class-methods-use-this": 0,
+    "import/prefer-default-export": 0,
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".ts", ".jsx"] }],
+    "prettier/prettier": ["error"],
   },
   parserOptions: {
     ecmaVersion: 6,
-    sourceType: 'module',
+    sourceType: "module",
     ecmaFeatures: {
       jsx: true,
     },
   },
-}
+};
