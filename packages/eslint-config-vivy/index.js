efd43a531e70b030d99ff15ab5443e341bdb778e
Ahmed Hassanein <ahmed.hassanein@vivy.com>
2020-05-19 13:03:09 +0200

    fixes and support for TS

--- packages/eslint-config-vivy/index.js
+++ packages/eslint-config-vivy/index.js
@@ -1,19 +1,24 @@
 module.exports = {
-  parser: 'babel-eslint',
+  parser: '@typescript-eslint/parser',
   extends: [
     'airbnb',
     'prettier',
     'prettier/react',
-    "plugin:jest/recommended"
-],
-  "plugins": [
-    "prettier",
-    "jest"
+    'plugin:react/recommended',
+    'plugin:jest/recommended',
   ],
+  plugins: ['prettier', 'jest', 'react'],
   rules: {
-    "class-methods-use-this": 0,
-    "import/prefer-default-export": 0,
-    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-    "prettier/prettier": ["error"]
-  }
+    'class-methods-use-this': 0,
+    'import/prefer-default-export': 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.ts', '.jsx'] }],
+    'prettier/prettier': ['error'],
+  },
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
 }
